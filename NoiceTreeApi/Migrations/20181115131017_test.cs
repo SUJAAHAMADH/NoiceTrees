1b566be4e2af0b99ac3421c5a86e05aef3dcbd0c
SUJAAHAMADH <sujasgk2@gmail.com>
2018-11-15 18:49:17 +0530

    commit1

---
+++ NoiceTreeApi/Migrations/20181115131017_test.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NoiceTreeApi.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NoiceTrees",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Type = table.Column<string>(nullable: true),
+                    JsonFormat = table.Column<string>(nullable: true),
+                    CreatedDate = table.Column<string>(nullable: true),
+                    ModifiedDate = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NoiceTrees", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NoiceTrees");
+        }
+    }
+}
---
+++ NoiceTreeApi/Model/NoiceTree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoiceTreeApi.Model
+{
+    public class NoiceTree
+    {
+        public long Id { get; set; }
+        public string Type { get; set; }
+        public string JsonFormat { get; set; }
+        public string CreatedDate { get; set; }
+        public string ModifiedDate { get; set; }
+    }
+}
